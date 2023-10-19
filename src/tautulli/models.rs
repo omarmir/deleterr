e12e75ade70f2f93b5be464e6a94ea9a7c56dcf8
Omar Mir <omarmir@gmail.com>
2023-10-19 18:02:44 +0000

    Tautulli data set to option There may be no watch history on the item

--- src/tautulli/models.rs
+++ src/tautulli/models.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use serde_aux::prelude::deserialize_option_number_from_string;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TautulliResponse {
@@ -14,7 +15,7 @@ pub struct ResponseContent {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ResponseData {
-    data: Vec<UserWatchHistory>,
+    data: Option<Vec<UserWatchHistory>>,
 }
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -30,8 +31,10 @@ pub struct UserWatchHistory {
     friendly_name: String,
     user_id: u64, // same is plex_id on tautulli
     full_title: String,
-    watched_status: u8, //0: Unwatched and 1: Watched
+    watched_status: f32, //0: Unwatched or less than half, 0.5: watched more than 50%, and 1: Watched
     rating_key: u64,
+    #[serde(deserialize_with = "deserialize_option_number_from_string")]
     parent_rating_key: Option<u64>,
+    #[serde(deserialize_with = "deserialize_option_number_from_string")]
     grandparent_rating_key: Option<u64>,
 }
