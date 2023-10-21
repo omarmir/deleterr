d7778b23cfdf58e9206935ca495553ebdac21c42
Omar <omarmir@gmail.com>
2023-10-21 15:07:42 -0400

    base implementation of windmill ui

--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -1,11 +1,61 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    themeVariants: ['dark'],
+    customForms: (theme) => ({
+      default: {
+        'input, textarea': {
+          '&::placeholder': {
+            color: theme('colors.gray.400'),
+          },
+        },
+      },
+    }),
+    extend: {
+      maxHeight: {
+        '0': '0',
+        xl: '36rem',
+      },
+      boxShadow: {
+        'outline-purple': '0 0 0 3px rgba(202, 191, 253, 0.45)',
+      }
+    },
   },
-  plugins: [],
+  variants: {
+    backgroundColor: [
+      'hover',
+      'focus',
+      'active',
+      'odd',
+      'dark',
+      'dark:hover',
+      'dark:focus',
+      'dark:active',
+      'dark:odd',
+    ],
+    display: ['responsive', 'dark'],
+    textColor: [
+      'focus-within',
+      'hover',
+      'active',
+      'dark',
+      'dark:focus-within',
+      'dark:hover',
+      'dark:active',
+    ],
+    placeholderColor: ['focus', 'dark', 'dark:focus'],
+    borderColor: ['focus', 'hover', 'dark', 'dark:focus', 'dark:hover'],
+    divideColor: ['dark'],
+    boxShadow: ['focus', 'dark:focus'],
+  },
+  plugins: [
+    require('tailwindcss-multi-theme'),
+    require('@tailwindcss/forms'),
+  ],
 }
