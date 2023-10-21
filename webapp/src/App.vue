93224f4838c64758da2971c9fb4b683f0fc1ad69
Omar <omarmir@gmail.com>
2023-10-21 14:29:17 -0400

    Added windmillui template

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="text-green-700 text-xl">This is a thing</div>
+  <div class="flex h-screen bg-gray-50 dark:bg-gray-900">
+    <Sidebar></Sidebar>
+    <div class="flex flex-col flex-1 w-full pb-2">
+      <Header />
+      <Dashboard />
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<script lang='ts' setup>
+import Sidebar from './components/Sidebar.vue'
+import Header from './components/Header.vue'
+import Dashboard from './views/Dashboard.vue'
+</script>
 
-</style>
+<style scoped></style>
