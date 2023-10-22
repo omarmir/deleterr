cc344dd12b42816a8995a9edff4dc3faf2436cc3
Omar <omarmir@gmail.com>
2023-10-22 00:31:07 -0400

    Simplifying UI Added v-if's instead of the default alpine Removed unsued deps Switched default to darkmode (light hurts eyes)

--- webapp/src/components/Header.vue
+++ webapp/src/components/Header.vue
@@ -29,12 +29,12 @@
         <!-- Theme toggler -->
         <li class="flex">
           <button class="rounded-md focus:outline-none focus:shadow-outline-purple" aria-label="Toggle color mode">
-            <template x-if="!dark">
+            <template v-if="!isDark">
               <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
               </svg>
             </template>
-            <template x-if="dark">
+            <template v-if="isDark">
               <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                 <path fill-rule="evenodd"
                   d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
@@ -56,7 +56,7 @@
             <span aria-hidden="true"
               class="absolute top-0 right-0 inline-block w-3 h-3 transform translate-x-1 -translate-y-1 bg-red-600 border-2 border-white rounded-full dark:border-gray-800"></span>
           </button>
-          <template x-if="isNotificationsMenuOpen">
+          <template v-if="isNotificationsMenuOpen">
             <ul x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100"
               x-transition:leave-end="opacity-0"
               class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:text-gray-300 dark:border-gray-700 dark:bg-gray-700">
@@ -97,7 +97,7 @@
               src="https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=aa3a807e1bbdfd4364d1f449eaa96d82"
               alt="" aria-hidden="true" />
           </button>
-          <template x-if="isProfileMenuOpen">
+          <template v-if="isProfileMenuOpen">
             <ul x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100"
               x-transition:leave-end="opacity-0"
               class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:border-gray-700 dark:text-gray-300 dark:bg-gray-700"
@@ -145,3 +145,12 @@
     </div>
   </header>
 </template>
+
+<script lang='ts' setup>
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+let isDark: Ref<boolean> = ref(false)
+let isNotificationsMenuOpen: Ref<Boolean> = ref(false)
+let isProfileMenuOpen: Ref<Boolean> = ref(false)
+</script>
--- webapp/tailwind.config.js
+++ webapp/tailwind.config.js
@@ -2,21 +2,12 @@
 const plugin = require('tailwindcss/plugin')
 
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    themeVariants: ['dark'],
-    customForms: (theme) => ({
-      default: {
-        'input, textarea': {
-          '&::placeholder': {
-            color: theme('colors.gray.400'),
-          },
-        },
-      },
-    }),
     extend: {
       maxHeight: {
         '0': '0',
@@ -27,35 +18,5 @@ export default {
       }
     },
   },
-  variants: {
-    backgroundColor: [
-      'hover',
-      'focus',
-      'active',
-      'odd',
-      'dark',
-      'dark:hover',
-      'dark:focus',
-      'dark:active',
-      'dark:odd',
-    ],
-    display: ['responsive', 'dark'],
-    textColor: [
-      'focus-within',
-      'hover',
-      'active',
-      'dark',
-      'dark:focus-within',
-      'dark:hover',
-      'dark:active',
-    ],
-    placeholderColor: ['focus', 'dark', 'dark:focus'],
-    borderColor: ['focus', 'hover', 'dark', 'dark:focus', 'dark:hover'],
-    divideColor: ['dark'],
-    boxShadow: ['focus', 'dark:focus'],
-  },
-  plugins: [
-    require('tailwindcss-multi-theme'),
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [],
 }
