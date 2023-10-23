a0befbef8d36ec04a5957c2bd4a871da30335165
Omar <omarmir@gmail.com>
2023-10-23 12:11:32 -0400

    Grid is more appropriate for services

--- webapp/src/components/Service.vue
+++ webapp/src/components/Service.vue
@@ -1,16 +1,12 @@
 <template>
-  <div class="basis-1/2">
+  <div>
     <h4 class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
       <img :src="logo" class="mr-2 inline h-6 w-6" />
       <slot></slot>
     </h4>
-    <div
-      class="mb-8 flex flex-col space-y-6 rounded-lg bg-white px-4 py-3 shadow-md dark:bg-gray-800">
+    <div class="flex flex-col space-y-6 rounded-lg bg-white px-4 py-3 shadow-md dark:bg-gray-800">
       <InputsInput label="Host" placeholder="Host"></InputsInput>
-      <InputsInput
-        label="Port"
-        placeholder="Port"
-        :type="InputType.number"></InputsInput>
+      <InputsInput label="Port" placeholder="Port" :type="InputType.number"></InputsInput>
       <InputsInput label="API Key" placeholder="API Key"></InputsInput>
       <InputsCheckbox>Use SSL</InputsCheckbox>
       <div class="flex justify-end space-x-4">
--- webapp/src/views/Services.vue
+++ webapp/src/views/Services.vue
@@ -1,15 +1,12 @@
 <template>
-  <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-    Services
-  </h2>
-  <div class="flex flex-row lg:space-x-6">
+  <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Services</h2>
+  <div class="grid gap-8 lg:grid-cols-2">
     <Service logo="/overseerr.svg">Overseerr</Service>
     <Service logo="/tautulli.png">Tautulli</Service>
-  </div>
-  <div class="flex flex-row lg:space-x-6">
     <Service logo="/sonarr.svg">Sonarr</Service>
     <Service logo="/radarr.svg">Radarr</Service>
   </div>
+  <div class="grid lg:grid-cols-2 lg:space-x-6"></div>
 </template>
 <script lang="ts" setup>
 import Service from '~/components/Service.vue'
