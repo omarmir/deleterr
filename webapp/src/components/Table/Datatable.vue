d3bc07552e265da724240b27b6d10a4e3847a604
Omar Mir <omarmir@gmail.com>
2023-10-29 22:57:47 -0400

    Generalized table heading and added icon Icon is not yet functional - neither is sort

--- webapp/src/components/Table/Datatable.vue
+++ webapp/src/components/Table/Datatable.vue
@@ -5,12 +5,12 @@
         <thead>
           <tr
             class="border-b bg-gray-50 text-left text-xs font-semibold uppercase tracking-wide text-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400">
-            <th class="px-4 py-3">Media</th>
-            <th class="px-4 py-3">Requested</th>
-            <th class="px-4 py-3">Type</th>
-            <th class="px-4 py-3">Watched</th>
-            <th class="px-4 py-3">Plex Username</th>
-            <th class="px-4 py-3">Actions</th>
+            <TableHeading>Media</TableHeading>
+            <TableHeading>Requested</TableHeading>
+            <TableHeading>Type</TableHeading>
+            <TableHeading>Watched</TableHeading>
+            <TableHeading>Plex Username</TableHeading>
+            <TableHeading :sort-icon="false">Actions</TableHeading>
           </tr>
         </thead>
         <tbody class="divide-y bg-white dark:divide-gray-700 dark:bg-gray-800">
@@ -22,8 +22,9 @@
 </template>
 <script lang="ts" setup>
 import { PropType } from 'vue'
-import DatatableRow from '~/components/Table/DatatableRow.vue'
 import { RequestStatus } from '~/@types/deleterr.ts'
+import DatatableRow from '~/components/Table/DatatableRow.vue'
+import TableHeading from '~/components/Table/Heading.vue'
 
 defineProps({
   requests: { required: false, type: Array as PropType<RequestStatus[]> },
---
+++ webapp/src/components/Table/Heading.vue
@@ -0,0 +1,23 @@
+<template>
+  <th class="cursor-pointer px-4 py-3" alt="Sort" aria-sort="none">
+    <div class="flex gap-2">
+      <slot></slot>
+      <div v-if="sortIcon" class="h-4 w-4 align-middle">
+        <svg
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="false">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5"></path>
+        </svg>
+      </div>
+    </div>
+  </th>
+</template>
+<script lang="ts" setup>
+defineProps({
+  sortIcon: { type: Boolean, required: false, default: true },
+})
+</script>
