f891ad4b71d3646457597dfde0e8a8c86bb7eea5
Omar Mir <omarmir@gmail.com>
2023-11-15 18:30:13 +0000

    Added session middlware

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 use actix_cors::Cors;
+use actix_session::SessionMiddleware;
+use actix_session::config::{BrowserSession, CookieContentSecurity};
+use actix_session::storage::CookieSessionStore;
+use actix_web::cookie::{Key, SameSite};
 use actix_web::{middleware::Logger, web, App, HttpServer};
 use actix_web_lab::web as lab_web;
 use deleterr::{endpoints as dr_epts, models::AppData, services as dr_serv};
@@ -15,6 +19,17 @@ mod overseerr;
 mod radarr;
 mod store;
 mod tautulli;
+
+fn session_middleware() -> SessionMiddleware<CookieSessionStore> {
+    SessionMiddleware::builder(CookieSessionStore::default(), Key::generate())
+    .cookie_name(String::from("deleterr"))
+    .cookie_secure(false) // TODO: Change this for prod
+    .session_lifecycle(BrowserSession::default())
+    .cookie_same_site(SameSite::Strict)
+    .cookie_content_security(CookieContentSecurity::Signed)
+    .cookie_http_only(true)
+    .build()
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -37,6 +52,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(cors)
             .wrap(logger)
+            .wrap(session_middleware())
             .app_data(app_data.clone())
             .configure(dr_epts::config)
             .service(
