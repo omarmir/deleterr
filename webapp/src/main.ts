c3a88e2e0cd70a9b92a363d9410978e3140ba799
Omar Mir <omarmir@gmail.com>
2023-11-19 22:23:50 -0500

    Created child routes to seperate login

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -4,16 +4,39 @@ import '~/style.css'
 import Dashboard from '~/views/Dashboard.vue'
 import Services from '~/views/Services.vue'
 import NotFound from '~/views/404.vue'
+import Login from '~/views/Login.vue'
+import Page from '~/views/Page.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { createPinia } from 'pinia'
 
 const routes = [
-  { path: '/services', component: Services },
-  { path: '/', component: Dashboard },
   {
-    path: '/:catchAll(.*)',
-    component: NotFound,
-    name: 'NotFound',
+    path: '/',
+    component: Page,
+    children: [
+      {
+        path: '',
+        name: 'Dashboard',
+        component: Dashboard,
+        auth: true,
+      },
+      {
+        path: 'services',
+        name: 'Services',
+        component: Services,
+        auth: true,
+      },
+      {
+        path: ':catchAll(.*)',
+        component: NotFound,
+        name: 'NotFound',
+      },
+    ],
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
 ]
 
---
+++ webapp/src/views/Page.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex h-screen bg-gray-50 dark:bg-gray-900">
+    <Sidebar></Sidebar>
+    <div class="flex w-full flex-1 flex-col pb-2">
+      <Header />
+      <main class="h-full overflow-y-auto">
+        <div class="container mx-auto grid px-6">
+          <router-view></router-view>
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import Sidebar from '~/components/Sidebar.vue'
+import Header from '~/components/Header.vue'
+</script>
