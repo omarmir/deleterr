a9dd1ad313531d08e4c6cf4add0fa1ee5d86812e
Omar Mir <omarmir@gmail.com>
2023-11-20 12:42:46 -0500

    Created docker config for nginx for proxy

--- src/main.rs
+++ src/main.rs
@@ -73,7 +73,7 @@ async fn main() -> std::io::Result<()> {
                     .finish(),
             )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 8081))?
     .run()
     .await
 }
