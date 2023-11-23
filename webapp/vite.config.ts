6ee2189000dc9cdce248b678decc65982b73e11a
Omar Mir <omarmir@gmail.com>
2023-11-23 10:28:20 -0500

    Added HMR support for reverse proxy

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -4,11 +4,16 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js',
-            '~': path.resolve(__dirname, './src'),
-        },
+  plugins: [vue()],
+  server: {
+    hmr: {
+      clientPort: 5111,
     },
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.esm-bundler.js',
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
 })
