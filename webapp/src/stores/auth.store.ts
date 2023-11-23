79b313da86d5de3eb1334dd8e646071757463813
Omar Mir <omarmir@gmail.com>
2023-11-23 10:49:37 -0500

    Persisting logged in user in sessionstore

--- webapp/src/stores/auth.store.ts
+++ webapp/src/stores/auth.store.ts
@@ -5,7 +5,7 @@ import { useRouter } from 'vue-router'
 
 export const useAuthStore = defineStore('auth', () => {
   const username: Ref<string | undefined> = ref(undefined)
-  const isLoggedIn: Ref<boolean> = ref(false)
+  const isLoggedIn: Ref<boolean> = ref(sessionStorage.getItem('loggedUser') ? true : false)
   const originalPath: Ref<string | undefined> = ref(undefined)
   const router = useRouter()
 
@@ -25,19 +25,23 @@ export const useAuthStore = defineStore('auth', () => {
       const response = await fetch(loginEndpoint, requestOptions)
       let apiResponse: APIResponse<string> = await response.json()
       if (apiResponse.success) {
-        [username.value, isLoggedIn.value] = [apiResponse.data ?? '', true]
+        ;[username.value, isLoggedIn.value] = [apiResponse.data ?? '', true]
+        sessionStorage.setItem('loggedUser', apiResponse.data ?? '')
         router.push(originalPath.value ?? '/')
         originalPath.value = undefined
       } else {
         console.log(apiResponse.error_msg)
+        sessionStorage.removeItem('loggedUser')
       }
     } catch (err) {
       console.log((err as any).toString())
+      sessionStorage.removeItem('loggedUser')
     }
   }
 
   async function logout() {
-    [username.value, isLoggedIn.value] = [undefined, false]
+    ;[username.value, isLoggedIn.value] = [undefined, false]
+    sessionStorage.removeItem('loggedUser')
   }
 
   return { isLoggedIn, username, login, logout, originalPath }
