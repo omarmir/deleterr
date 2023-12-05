faba025fb429febe88a8657939408ae2ada61647
Omar Mir <omarmir@gmail.com>
2023-12-05 14:40:36 -0500

    Date formatting on ui

--- webapp/src/components/Requests/ListItems/NameRelease.vue
+++ webapp/src/components/Requests/ListItems/NameRelease.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col space-y-1 text-gray-700 dark:text-gray-400">
     <p class="font-semibold">{{ title ?? 'N/A' }}</p>
-    <p class="text-xs">Release: {{ releaseDate ?? 'N/A' }}</p>
+    <p class="text-xs">Release: {{ getDate(releaseDate) ?? 'N/A' }}</p>
     <p v-if="ended" class="text-xs text-red-500">Ended</p>
   </div>
 </template>
@@ -11,4 +11,7 @@ defineProps({
   releaseDate: { type: String, required: false },
   ended: { type: Boolean, required: false },
 })
+
+// YYYY-MM-DD - dont like it? Make your own app. Just be happy its not my preferred 24H
+const getDate = (newDate?: string) => (newDate ? new Date(newDate).toLocaleDateString('en-CA') : 'N/A')
 </script>
