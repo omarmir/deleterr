26d046b2490508f671c2aca7935bb214a96215b9
Omar Mir <omarmir@gmail.com>
2024-11-01 21:07:15 -0400

    Adding a warning notice for settings not working

---
+++ webapp/src/components/Warning.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="flex items-center justify-between rounded-lg bg-orange-600 p-4 text-sm font-semibold text-white shadow-md focus:shadow-outline-purple focus:outline-none"
+    target="_blank">
+    <div class="flex items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="mr-2 size-5">
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+      </svg>
+
+      <slot></slot>
+    </div>
+  </div>
+</template>
--- webapp/src/views/Settings.vue
+++ webapp/src/views/Settings.vue
@@ -2,12 +2,13 @@
   <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">Settings</h2>
   <div class="flex flex-col space-y-5">
     <CTA action-link="https://github.com/omarmir/deleterr">
-      <span>This is ALPHA software. Features are unstable. Please report any issues on GitHub!</span>
+      <span>This is PRE-ALPHA software. Features are unstable. Please report any issues on GitHub!</span>
       <template #action>
         <span>GitHub &RightArrow;</span>
       </template>
     </CTA>
     <NewPassword></NewPassword>
+    <Warning>None of the settings below work yet as this is still under development.</Warning>
     <Suspense>
       <Settings />
       <template #fallback>
@@ -22,6 +23,7 @@
 <script lang="ts" setup>
 import PageLoading from '~/components/PageLoading.vue'
 import NewPassword from '~/components/NewPassword.vue'
+import Warning from '~/components/Warning.vue'
 import CTA from '~/components/CTA.vue'
 import Settings from '~/components/Settings.vue'
 </script>
